8397b039e275908dd51deda22be32b65fbeee494
Đinh Hữu Nhật Minh <yuumiyuumi2005@gmail.com>
2026-04-26 16:00:33 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 const express = require('express')
 const path = require('path')
-
+const helmet = require('helmet')
 const port = process.env.PORT || 5006
 
 const app = express()
-
+app.use(helmet())
 app.use(express.static(path.join(__dirname, 'public')))
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
@@ -19,12 +19,7 @@ const server = app.listen(port, () => {
   console.log(`Listening on ${port}`)
 })
 
-// The number of seconds an idle Keep-Alive connection is kept open. This should be greater than the Heroku Router's
-// Keep-Alive idle timeout of 90 seconds:
-// - to ensure that the closing of idle connections is always initiated by the router and not the Node.js server
-// - to prevent a race condition if the router sends a request to the app just as Node.js is closing the connection
-// https://devcenter.heroku.com/articles/http-routing#keepalives
-// https://nodejs.org/api/http.html#serverkeepalivetimeout
+
 server.keepAliveTimeout = 95 * 1000
 
 process.on('SIGTERM', async () => {
